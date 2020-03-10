5d4aa118cd3a963f50ab97d1a3b97bf70aa4623d
cdixson <crystal.dixson@gmail.com>
2020-03-10 14:53:38 -0700

    rename woes

--- my_lambdata/split.py
+++ my_lambdata/split.py
@@ -1,7 +1,7 @@
 import pandas
 from sklearn.model_selection import train_test_split
 
-import cdixson-ds-lambdata-12==1.0
+#import cdixson-ds-lambdata-12==1.0
 
 def split(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.22, random_state=42)
