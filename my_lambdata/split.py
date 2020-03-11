568c5f7553f085774dfc22809a192a233722ff4f
cdixson <crystal.dixson@gmail.com>
2020-03-11 10:29:30 -0700

    State function

--- my_lambdata/split.py
+++ my_lambdata/split.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 
 def split(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.22, random_state=42)
